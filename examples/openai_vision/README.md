3b2aef0ce28dd88c455703568efcf29993876f03
jeduardofr <jeduardo.fuentes@alumnos.udg.mx>
2024-08-12 23:34:21 -0600

    feat: openai vision example

---
+++ examples/openai_vision/README.md
@@ -0,0 +1,19 @@
+# Vision from OpenAI
+
+Version de python recomendada 3.11.4 y pip 24.0
+
+## Instalar dependencias
+
+    pip install -r requirements.txt
+
+## Crear variables de entorno
+
+Generar un archivo `.env` en base al template de ejemplo:
+
+    cp .env.example .env
+
+Modificar el archivo `.env` con las credenciales de OpenAI.
+
+## Correr demo
+
+    python demo.py
---
+++ examples/openai_vision/demo.py
@@ -0,0 +1,51 @@
+import base64
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# OpenAI API Key
+api_key = os.getenv("OPENAI_API_KEY")
+
+# Function to encode the image
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+# Path to your image
+image_path = os.path.join('..', '..', 'assets', 'aerial-drone-pov.jpg')
+
+# Getting the base64 string
+base64_image = encode_image(image_path)
+
+headers = {
+  "Content-Type": "application/json",
+  "Authorization": f"Bearer {api_key}"
+}
+
+payload = {
+  "model": "gpt-4o-mini",
+  "messages": [
+    {
+      "role": "user",
+      "content": [
+        {
+          "type": "text",
+          "text": "What’s in this image?"
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": f"data:image/jpeg;base64,{base64_image}"
+          }
+        }
+      ]
+    }
+  ],
+  "max_tokens": 300
+}
+
+response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+print(response.json())
